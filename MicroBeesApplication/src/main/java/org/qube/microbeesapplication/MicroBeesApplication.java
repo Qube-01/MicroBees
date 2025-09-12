8c3f7fa685d9b85cd88e8f1fbb90ded6b874aabc
Kousik Das <kousikd2003@gmail.com>
2025-09-12 17:46:26 +0530

    Added initial dependencies

---
+++ MicroBeesApplication/src/main/java/org/qube/microbeesapplication/MicroBeesApplication.java
@@ -0,0 +1,13 @@
+package org.qube.microbeesapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroBeesApplication.class, args);
+    }
+
+}
---
+++ MicroBeesApplication/src/test/java/org/qube/microbeesapplication/MicroBeesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qube.microbeesapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
